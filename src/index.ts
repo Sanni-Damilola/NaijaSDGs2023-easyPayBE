b418c636548880de0ea505844ad1c2a2a5bf6d84
Sanni-Damilola <sannifortune11@gmail.com>
2023-11-29 08:28:54 +0100

    added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dbConfig from "../config/db";
-import adminAuthRoutes from "../router/admin/adminAuth";
-import staffAuthRoutes from "../router/staff/staffAuthRoutes";
-import AdminRoutes from "../router/admin/adminRoutes";
-import staffRoutes from "../router/staff/staffRoutes";
 
 const PORT = process.env.PORT || 2023;
 
@@ -21,18 +17,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 //routes
-
-//admin authentication routes
-app.use("/admin", adminAuthRoutes);
-
-//staff authentication routes
-app.use("/staff", staffAuthRoutes);
-
-//admin dashboard routes
-app.use("/pay" ,AdminRoutes)
-
-//staff dashboard routes
-app.use("/plans" , staffRoutes)
 
 app.listen(PORT, () => {
   console.log(`server is up on port ${PORT}`);
